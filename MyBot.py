dfa75a5e5e29d7e1f54f7ed1a368382e0b654637
Rob Sokolowski <rpsoko@gmail.com>
2018-10-28 22:54:40 -0400

    seek home works, still drunk tho

--- MyBot.py
+++ MyBot.py
@@ -22,36 +22,63 @@ import logging
 
 # This game object contains the initial game state.
 game = hlt.Game()
-# At this point "game" variable is populated with initial map data.
-# This is a good place to do computationally expensive start-up pre-processing.
-# As soon as you call "ready" function below, the 2 second per turn timer will start.
+
+# Do pre-processing here.
+game_map = game.game_map
+me = game.me
+
+
+# Ready starts the 2 second clock
 game.ready("Crush_v0.1")
 
 # Now that your bot is initialized, save a message to yourself in the log file with some important information.
 #   Here, you log here your id, which you can always fetch from the game object by using my_id.
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
-""" <<<Game Loop>>> """
+
+def log_state(me, game_map):
+    logging.info(f"halite_amount={me.halite_amount}")
+    ships = me.get_ships()
+    for ship in ships:
+        logging.info({ship})
+
+ship_values = {}
 
 while True:
     # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
     #   running update_frame().
+    command_queue = []
     game.update_frame()
-    # You extract player metadata and the updated map metadata here for convenience.
     me = game.me
     game_map = game.game_map
+    my_ships = me.get_ships()
+    shipyard = me.shipyard
 
-    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-    #   end of the turn.
-    command_queue = []
+    log_state(me=me, game_map=game_map)
 
-    for ship in me.get_ships():
+    for ship in my_ships:
+        if ship.id not in ship_values:
+            ship_values[ship.id] = {'seeking_home': False}
         # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
         #   Else, collect halite.
-        if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
-            command_queue.append(
-                ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+        if ship.is_full and not ship_values[ship.id]['seeking_home']:
+            # This ship is now seeking the shipyard and will continue to do so despite the cost!
+            ship_values[ship.id]['seeking_home'] = True
+            logging.info(f"ship {ship.id} is full, now set to seeking shipyard at {shipyard.position}")
+            direction = game_map.naive_navigate(ship, shipyard.position)
+            command_queue.append(ship.move(direction))
+        elif ship_values[ship.id]['seeking_home'] and ship.position == shipyard.position:
+            logging.info(f"ship {ship.id} has reached the shipyard. No longer seeking it")
+            ship_values[ship.id]['seeking_home'] = False
+            # Move in a random dir
+            command_queue.append(ship.move(random.choice([Direction.North, Direction.South, Direction.East, Direction.West])))
+        elif ship_values[ship.id]['seeking_home']:
+            logging.info(f"ship {ship.id} is continuing to seek shipyard at {shipyard.position}")
+            direction = game_map.naive_navigate(ship, shipyard.position)
+            command_queue.append(ship.move(direction))
+        elif game_map[ship.position].halite_amount < constants.MAX_HALITE / 10:
+            # Move in a random dir
+            command_queue.append(ship.move(random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
         else:
             command_queue.append(ship.stay_still())
 
