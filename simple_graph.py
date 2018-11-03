e9d26f28aab78b96f9a5c4f3572934e5574d947d
Rob Sokolowski <rpsoko@gmail.com>
2018-11-03 17:42:13 -0400

    testing mom's mac

---
+++ simple_graph.py
@@ -0,0 +1,12 @@
+class Graph:
+	def __init__(self):
+		self._nodes = {}
+		self._node_count = 0
+
+	def add_node(self, node):
+		pass
+
+class Node:
+	def __init__(self, value):
+		self._value = value
+
