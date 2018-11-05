338806efb6ace00b7c2ee79d0f539154de3fb814
Rob Sokolowski <rpsoko@gmail.com>
2018-11-04 23:36:39 -0500

    basic dijkstra impl

--- dijkstra.py
+++ dijkstra.py
@@ -1,31 +1,57 @@
-def dijkstra(graph, src):
+import heapq
+
+
+def dijkstra(graph, src, dest):
     """
-    Following's Skiena's Algorithm Design manual, ch. 6.3. This finds shortest path from src to all other nodes. I don't
-    think this is what I want for halite, but should be a good start.
+    Originally I started following Skiena's implementation of Dijkstra's algorithm, but it wasn't quite what I wanted.
 
-    :param graph:
-    :param src:
-    :return:
+    I'm not sure if this technically is still Dijkstra, but it seems to be doing what I want.
 
-    TODO: Sec 6.3.2 "All-Pairs shortest path may be optimal for deploying drop-offs
+    Note: Works for non-negative wieghts only!
     """
+    nodes = graph.get_nodes()
+    path_heap = [_Path([src], 0.0)]
+    heapq.heapify(path_heap)
 
-    intree = {}
-    distance = {}
-    parent = {}
+    completed_path_heap = []
+    heapq.heapify(completed_path_heap)
 
-    for i in range(0, graph.get_node_count()):
-        intree[i] = False
-        distance[i] = float('inf')
-        parent[i] = -1
+    while path_heap:
+        path = heapq.heappop(path_heap)
+        current = path.nodes[-1]
+        if current == dest:
+            heapq.heappush(completed_path_heap, path)
+        current_node = nodes[current]
+        neighbors = current_node.get_neighbors()
 
-    distance[src] = 0
-    v = src
+        for neighbor in neighbors:
+            if neighbor in path.nodes:  # TODO: Is `in` on Python Lists O(n)???
+                continue
+            new_nodes = path.nodes + [neighbor]
+            new_cost = path.cost + current_node.get_weight(neighbor)
+            new_path = _Path(nodes=new_nodes, cost=new_cost)
+            heapq.heappush(path_heap, new_path)
 
-    while intree[v] is False:
-        intree[v] = True
-        for p in v.get_neighbors():
-            weight = v.get_weight(p)
-            if distance[p] > distance[src] + weight:
-                distance[p] = distance[src] + weight
-                parent[p] = v
+    best_path = heapq.heappop(completed_path_heap)
+    return best_path.nodes[1:]
+
+
+class _Path:
+    """
+    class to keep track of a path on a heapq. Only the cost is considered when placing on the heap
+    """
+    def __init__(self, nodes, cost):
+        self.nodes = nodes
+        self.cost = cost
+
+    def __gt__(self, other):
+        return self.cost > other.cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
+
+    def __eq__(self, other):
+        return self.cost == other.cost
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
